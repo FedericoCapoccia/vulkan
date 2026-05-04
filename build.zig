891b83dadbfe4263ad7121734ec5cd0734d3a8d7
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-05-04 22:17:54 +0200

    added deps [pain]

--- build.zig
+++ build.zig
@@ -4,15 +4,43 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml");
+    const vk_gen = b.dependency("vulkan", .{}).artifact("vulkan-zig-generator");
+
+    const vk_generate_cmd = b.addRunArtifact(vk_gen);
+    vk_generate_cmd.addFileArg(registry);
+
+    const vulkan_zig = b.addModule("vulkan-zig", .{
+        .root_source_file = vk_generate_cmd.addOutputFileArg("vk.zig"),
+    });
+
+    const zglfw = b.dependency("zglfw", .{
+        .target = target,
+        .optimize = optimize,
+        .import_vulkan = true,
+        .x11 = false,
+        .wayland = true,
+    });
+
+    const zglfw_mod = zglfw.module("root");
+    zglfw_mod.addImport("vulkan", vulkan_zig);
+
     const mod = b.addModule("vulkan", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    mod.addImport("vulkan", vulkan_zig);
+    mod.addImport("zglfw", zglfw_mod);
+    if (target.result.os.tag != .emscripten) {
+        mod.linkLibrary(zglfw.artifact("glfw"));
+    }
+
     const exe = b.addExecutable(.{
         .name = "vulkan",
         .root_module = mod,
+        .use_llvm = true,
     });
 
     b.installArtifact(exe);
@@ -20,6 +48,7 @@ pub fn build(b: *std.Build) void {
     const exe_c = b.addExecutable(.{
         .name = "vulkan",
         .root_module = mod,
+        .use_llvm = true,
     });
 
     const run_step = b.step("run", "Run the app");
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
 const Io = std.Io;
 
+const glfw = @import("zglfw");
+const vk = @import("vulkan");
+
 pub fn main(_: std.process.Init) !void {}
